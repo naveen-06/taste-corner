ae00d6b16425f9572147a9ee90e27af3e89d86ea
naveen-06 <70632802+naveen-06@users.noreply.github.com>
2021-03-09 04:44:27 -0500

    Add files via upload

---
+++ precache-manifest.9f1ea9a0f0073fa6af48e5ef8cd1e4ee.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8fbc16b27b5d082f3bb95a621befca7",
+    "url": "/taste-corner/index.html"
+  },
+  {
+    "revision": "04f2f62b5a31af3aef69",
+    "url": "/taste-corner/static/css/4.f5d2056d.chunk.css"
+  },
+  {
+    "revision": "0b55e6ecc11bd42fc006",
+    "url": "/taste-corner/static/css/5.c8c9a2d3.chunk.css"
+  },
+  {
+    "revision": "8c67bb7b3b0d5d83e024",
+    "url": "/taste-corner/static/css/main.ee92e6fa.chunk.css"
+  },
+  {
+    "revision": "4cdcb35e19c579a627b2",
+    "url": "/taste-corner/static/js/0.162921ca.chunk.js"
+  },
+  {
+    "revision": "f7ef4cd79e80f77f06ea",
+    "url": "/taste-corner/static/js/3.5214dc8e.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/taste-corner/static/js/3.5214dc8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04f2f62b5a31af3aef69",
+    "url": "/taste-corner/static/js/4.cf2d7ae9.chunk.js"
+  },
+  {
+    "revision": "0b55e6ecc11bd42fc006",
+    "url": "/taste-corner/static/js/5.4ba97b4a.chunk.js"
+  },
+  {
+    "revision": "918b242cb6363ea15851",
+    "url": "/taste-corner/static/js/6.35d4030c.chunk.js"
+  },
+  {
+    "revision": "8c67bb7b3b0d5d83e024",
+    "url": "/taste-corner/static/js/main.12613003.chunk.js"
+  },
+  {
+    "revision": "efdcb672f4a40433d76b",
+    "url": "/taste-corner/static/js/runtime-main.1cd31795.js"
+  },
+  {
+    "revision": "c1e769605e4686003967900cb433dc65",
+    "url": "/taste-corner/static/media/facebook.c1e76960.svg"
+  },
+  {
+    "revision": "fc23b90236f87b11147810f35f4c5293",
+    "url": "/taste-corner/static/media/instagram.fc23b902.svg"
+  },
+  {
+    "revision": "a57ea219084451288a1f02aabba16f1b",
+    "url": "/taste-corner/static/media/twitter.a57ea219.svg"
+  }
+]);
